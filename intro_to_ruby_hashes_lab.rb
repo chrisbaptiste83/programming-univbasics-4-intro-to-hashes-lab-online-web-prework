49b77c314fafa200f37b0dced3a4f515c925b9e0
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-06-29 09:24:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,7 @@ pioneer = { :name => 'Grace Hopper'}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
-end
+ id = { 
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
